1d6b4815cfc98b3a8b928cf20dc4acddcf0350f8
ganewman42 <ganewman42@gmail.com>
2016-05-09 17:59:49 -0400

    Initial Commit

---
+++ NewGitProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewGitProject
+//
+//  Created by Grace Newman on 5/9/16.
+//  Copyright © 2016 Grace Newman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
